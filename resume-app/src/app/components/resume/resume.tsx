17501e4c11d648f97aefbb499e22ff2105e517a5
Jony Oinas <jonyoinas@gmail.com>
2023-05-09 14:55:21 -0700

    Fixed resume css grid

--- resume-app/src/app/components/resume/resume.tsx
+++ resume-app/src/app/components/resume/resume.tsx
@@ -1,8 +1,20 @@
+import './resume.css';
 
 export default function Resume() {
     return (
         <div className="Resume">
-            Resume
+            <div className="ResumeLeftPanel">
+                Left
+            </div>
+            <div className="ResumeMiddleLeft">
+                MiddleLeft
+            </div>
+            <div className="ResumeMiddleRight">
+                MiddleRight
+            </div>
+            <div className="ResumeRightPanel">
+                Right
+            </div>
         </div>
     )
 }
