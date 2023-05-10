17f4c41c99afd997ee278347c817b4ff6720fa1a
Jony Oinas <jonyoinas@gmail.com>
2023-05-10 13:36:06 -0700

    resume adjustments

--- resume-app/src/app/components/resume/resume.tsx
+++ resume-app/src/app/components/resume/resume.tsx
@@ -3,18 +3,20 @@ import './resume.css';
 export default function Resume() {
     return (
         <div className="Resume">
-            <div className="ResumeLeftPanel">
-                Left
+            <div className="resumeLeftContainer-1">
+                <div className="resumeLeftPanelHeader">
+                    <h1>Jony <br /> Oinas</h1>
+                </div>
             </div>
-            <div className="ResumeMiddleLeft">
-                MiddleLeft
-            </div>
-            <div className="ResumeMiddleRight">
-                MiddleRight
-            </div>
-            <div className="ResumeRightPanel">
-                Right
-            </div>
+            <div className="resumeRightContainer-1" />
+            <div className="resumeLeftContainer-2" />
+            <div className="resumeRightContainer-2" />
+            <div className="resumeLeftContainer-3" />
+            <div className="resumeRightContainer-3" />
+            <div className="resumeLeftContainer-4" />
+            <div className="resumeRightContainer-4" />
+            <div className="resumeLeftContainer-5" />
+            <div className="resumeRightContainer-5" />
         </div>
     )
 }
